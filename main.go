10103bcb0a71ecf25836f91b48b8541dc97b52af
Jimmy Morrison <81664212+jamesmorrison883@users.noreply.github.com>
2024-07-07 22:50:23 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/extism/go-pdk"
+)
+
+//go:export greet
+func Greet() int32 {
+	name := pdk.InputString()
+	pdk.OutputString("Hello, " + name)
+	return 0
+}
+func main() {}
